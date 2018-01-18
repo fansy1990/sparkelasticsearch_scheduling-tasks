3aa0db661b361188ab1d7e6b75cdbc8b768dce70
fansy <fansy1990@foxmail.com>
2018-01-18 11:49:51 +0800

    添加spark资源及调用方式；

--- src/main/java/com/jobs/DemoJob.java
+++ src/main/java/com/jobs/DemoJob.java
@@ -3,11 +3,8 @@ package com.jobs;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.config.DemoJobConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 /**
